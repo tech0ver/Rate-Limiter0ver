3a4f5281b379504141a4577db67163a1395e8ddf
Grig Alex <alexengrigdev@gmail.com>
2025-09-04 21:04:09 +0300

    Init spring boot

---
+++ src/main/java/io/github/tech0ver/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.tech0ver.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
